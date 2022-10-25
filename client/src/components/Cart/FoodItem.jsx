8f3c68873a65f314a609ccb1afd565f49348fe5c
Akash Koodali <akashkoodali9@gmail.com>
2022-10-25 10:42:36 +0530

    Completed UI Part

--- client/src/components/Cart/FoodItem.jsx
+++ client/src/components/Cart/FoodItem.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { BsTrashFill } from "react-icons/bs";
 
-const FoodItem = () => {
+const FoodItem = (props) => {
+  const deleteFoodFromCart = () => {};
+
+  const increment = () => {};
+
+  const decrement = () => {};
+
   return (
-    <div>FoodItem</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-between">
+        <h5>{props.name}</h5>
+        <div className="flex items-end gap-2">
+          <div className="flex flex-col items-end">
+            <small>₹{parseInt(props.price) * parseInt(props.quantity)}</small>
+            <div className="px-1 bg-zomato-400 text-white rounded flex items-center gap-1">
+              <button
+                onClick={decrement}
+                className="p-1 bg-zomato-400 text-white rounded"
+              >
+                -
+              </button>
+              <small>{props.quantity}</small>
+              <button
+                onClick={increment}
+                className="p-1 bg-zomato-400 text-white rounded"
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="pb-2">
+            <BsTrashFill
+              onClick={deleteFoodFromCart}
+              className="text-zomato-400 text-lg md:text-xl cursor-pointer"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FoodItem
+export default FoodItem;
